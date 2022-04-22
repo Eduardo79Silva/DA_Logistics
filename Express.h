b5f8599b5269e035faef6540d5d13b62848cb1ae
Eduardo Silva <up202005283@up.pt>
2022-04-22 14:00:28 +0100

    Documented version

--- Express.h
+++ Express.h
@@ -10,11 +10,25 @@
 class Express {
 
 private:
+    /*
+     * Vector of vectors containing the result of the algorithm that chooses the best parcels taking into account their time
+     */
     inline static vector<vector<int>> chosenParcels;
 
 public:
+    /*
+     * Function that sorts the parcels by their time to deliver
+     * @param parcels - the vector of vectors that will be sorted
+     */
     static void sortParcels(vector<vector<int>> &parcels);
+    /*
+     * Algorithm in charge of choose the best parcels for that day (from 9AM to 5PM)
+     * @return number of chosen parcels
+     */
     static int chooseParcels(vector<vector<int>> sortedParcels);
+    /*
+     * Function that returns the chosen parcels
+     */
     static vector<vector<int>> getChosenParcels();
 
 };
